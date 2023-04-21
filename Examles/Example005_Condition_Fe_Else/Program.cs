1894d2a528d0839396da6181ebabb11361c3c1b1
Kulikov Alexander <shouter84@yandex.ru>
2023-04-21 22:24:39 +0300

    Added EXAMPLE 005 Condition if/Else

---
+++ Examles/Example005_Condition_Fe_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же Санек, как твои дела друг? Я скучал!");
+}
+else
+{
+    Console.Write("А ты кто такой? я тебя не знаю! Ну, привет: ");
+    Console.WriteLine(username);
+}
