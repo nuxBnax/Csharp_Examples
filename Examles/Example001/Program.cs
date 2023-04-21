8bcd8d337a33cf3707c831a54b1c8be87ddb1b58
Kulikov Alexander <shouter84@yandex.ru>
2023-04-21 22:02:24 +0300

    Debuging gitignore

---
+++ Examles/Example001/Program.cs
@@ -0,0 +1,4 @@
+﻿// Знакомимся с С#
+Console.WriteLine("Hello, World");
+Console.WriteLine("Hello, World and Alexander!");
+Console.Write("Hello, World and Misha!");
---
+++ Examles/Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter your name, please!");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
